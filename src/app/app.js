f9d36361b646c1156bd8a6a4142dfd1c784d66b2
sjbgit <sjb_strat@hotmail.com>
2015-02-24 19:17:12 -0600

    start tabs

--- src/app/app.js
+++ src/app/app.js
@@ -25,13 +25,41 @@
                 controller: 'LeaguesCtrl',
                 controllerAs: 'vm',
                 resolve: {
-                    initialData: ['eliteApi', function(eliteApi) {
+                    initialData: ['eliteApi', function (eliteApi) {
                         return eliteApi.getLeagues();
                     }]
                 }
             })
-
-            ;
+            .when('/leagues/:id/teams', {
+                templateUrl: 'app/teams/teams.html',
+                controller: 'TeamsCtrl',
+                controllerAs: 'vm',
+                resolve: {
+                    initialData: ['$route', 'eliteApi', function ($route, eliteApi) {
+                        return eliteApi.getTeams($route.current.params.id);
+                    }]
+                }
+            })
+            .when('/leagues/:id/games', {
+                templateUrl: 'app/games/games.html',
+                controller: 'GamesCtrl',
+                controllerAs: 'vm',
+                resolve: {
+                    initialData: ['$route', 'gamesInitialDataService', function ($route, gamesInitialDataService) {
+                        return gamesInitialDataService.getData($route.current.params.id);
+                    }]
+                }
+            })
+            .when('/leagues/:id/league-home', {
+                templateUrl: 'app/league-home/league-home.html',
+                controller: 'LeagueHomeCtrl',
+                controllerAs: 'vm',
+                resolve: {
+                    initialData: ['$route', 'eliteApi', function ($route, eliteApi) {
+                        return eliteApi.getLeague($route.current.params.id);
+                    }]
+                }
+            });
 
 
 
